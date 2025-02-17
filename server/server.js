b991cec97dc6b157efc32e9054585b799c84cb96
Milan Chaulagain <mchaulagain57@gmail.com>
2025-02-17 10:04:03 +0545

    updated name of mongodb.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import 'dotenv/config'
-import connectDB from "./configs/monodb.js"
+import connectDB from "./configs/mongodb.js"
 import { clerkWebhooks } from "./controllers/webhooks.js"
 
 //Initialize express
