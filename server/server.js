e2a956b936d5d3e5bce9d958b3bd1e8e21317a2f
Milan Chaulagain <mchaulagain57@gmail.com>
2025-04-28 08:47:35 +0545

    stripe added

--- server/server.js
+++ server/server.js
@@ -2,21 +2,35 @@ import express from "express"
 import cors from "cors"
 import 'dotenv/config'
 import connectDB from "./configs/mongodb.js"
-import { clerkWebhooks } from "./controllers/webhooks.js"
+import { clerkWebhooks, stripeWebhooks } from "./controllers/webhooks.js"
+import educatorRouter from "./routes/educatorRoutes.js"
+import { clerkMiddleware } from "@clerk/express"
+import connectCloudinary from "./configs/cloudinary.js"
+import courseRouter from "./routes/courseRoutes.js"
+import userRouter from "./routes/userRoutes.js"
 
 //Initialize express
 const app = express()
 
 //Connect to Database
 await connectDB()
+await connectCloudinary()
 
 //Middlewares
 
 app.use(cors())
+app.use(clerkMiddleware())
+// Other normal JSON routes
+app.use(express.json());
 
 //Routes
 app.get('/', (req, res)=> res.send("API working"))
 app.post('/clerk', express.json(), clerkWebhooks)
+app.post("/api/webhooks", express.raw({type: "*/*"}), clerkWebhooks);
+app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/course', express.json(), courseRouter)
+app.use('/api/user', express.json(), userRouter)
+app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
 //PORT
 const PORT = process.env.PORT || 5000
